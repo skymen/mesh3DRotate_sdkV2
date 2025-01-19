41868c32c6a03da03f3efbe16ba3acb52b24ef91
Ossama Jouini <skymen@users.noreply.github.com>
2025-01-19 23:33:16 +0900

    Initial commit

---
+++ src/runtime/instance.js
@@ -0,0 +1,30 @@
+import { id } from "../../config.caw.js";
+
+export default function (parentClass) {
+  return class extends parentClass {
+    constructor() {
+      super();
+      const properties = this._getInitProperties();
+      if (properties) {
+      }
+    }
+
+    _trigger(method) {
+      super._trigger(self.C3.Plugins[id].Cnds[method]);
+    }
+
+    _release() {
+      super._release();
+    }
+
+    _saveToJson() {
+      return {
+        // data to be saved for savegames
+      };
+    }
+
+    _loadFromJson(o) {
+      // load state for savegames
+    }
+  };
+}
